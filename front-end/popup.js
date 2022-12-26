ba3405efd4b7343a49f60665ab0f1fbf4c53bd9f
Nikoleta Dimitrova <n.dimitrova@student.fontys.nl>
2022-12-26 22:43:28 +0100

    login,signing and payment popup fixing

--- front-end/popup.js
+++ front-end/popup.js
@@ -1,59 +1,20 @@
-
-
-document.querySelector("#open-popup").addEventListener("click",async ()=>{
-    let response=await fetch("login.html")
-    let html_response=await response.text()
-    document.querySelector("div#popup").innerHTML=html_response
-})
-
-document.querySelector("button#close-popup").addEventListener("click", async ()=>{
-    const popup = document.getElementById("popup")
-    popup.innerHTML = '';
-})
-
-console.log(document.getElementById("lognin-signup-btn"))
-
-function loginToSignup(){
-    async function logTOsign(){ 
-        let response=await fetch("signin.html")
-        console.log(response)
-        let html_response=await response.text()
-        document.querySelector("div#popup").innerHTML=html_response
-    }
-    logTOsign()
+async function loadPopupContent(popup) {
+    const response = await fetch(popup);
+    const html_response = await response.text()
+    document.querySelector("div#popup").innerHTML = html_response
 }
 
-function signupToLogo(){
-    async function signTOlog(){ 
-        let response=await fetch("login.html")
-        console.log(response)
-        let html_response=await response.text()
-        document.querySelector("div#popup").innerHTML=html_response
-    }
-    signTOlog()
-}
-
-function loginToPayment(){
-    async function logTOpay(){ 
-        let response=await fetch("payment.html")
-        console.log(response)
-        let html_response=await response.text()
-        document.querySelector("div#popup").innerHTML=html_response
-    }
-    logTOpay()
-}
-
-function removeLogin(){
-    var element = document.getElementById("login-popup-container")
+function closeLoginPopup() {
+    const element = document.getElementById("login-popup-container")
     element.remove();
 }
 
-function removeSignin(){
-    var element = document.getElementById("signin-popup-container")
+function closeSigninPopup() {
+    const element = document.getElementById("signin-popup-container")
     element.remove();
 }
 
-function removePayment(){
-    var element = document.getElementById("payment-popup-container")
+function closePaymentPopup() {
+    const element = document.getElementById("payment-popup-container")
     element.remove();
 }
